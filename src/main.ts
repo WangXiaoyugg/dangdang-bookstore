0979eb762d4a28e5f61228599d1cfda98ee8a57a
xywang71 <xywang71@iflytek.com>
2023-06-07 10:39:59 +0800

    feat(init): 自定义环境变量

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+console.log('env', import.meta.env.VITE_Username, import.meta.env.VITE_Age);
+
 createApp(App).mount('#app')
